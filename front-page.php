960b1315cbb2a35d80d0e97d897b3a27f7cffd85
jhukal9@gmail.com <jhukal9@gmail.com>
2024-09-18 18:03:54 +0545

    updated the steps section

--- front-page.php
+++ front-page.php
@@ -2,6 +2,7 @@
 
 <?php 
 get_template_part('template-parts/landing-page/page-banner',null);
+get_template_part('template-parts/landing-page/steps',null);
 ?>
 
 <?php get_footer()?>
---
+++ template-parts/landing-page/steps.php
@@ -0,0 +1,60 @@
+<section class="steps section-gap">
+  <div class="container">
+    <div class="section-title">
+      <h2 class="text-center">Turning your Ultimate <span>Dream Trip into Reality</span></h2>
+      <p class="text-center">Choose the adventure that inspires you and embark on a journey</p>
+    </div>
+    <div class="steps-content-wrapper">
+      <div class="row">
+        <div class="col-md-6 col-lg-3">
+          <div class="steps-item d-flex justify-content-start align-items-start d-md-block">
+            <div class="step-icon">
+              <img src="<?php echo get_parent_theme_file_uri()?>/assets/images/icons/step-01.svg" alt=""
+                class="img-fluid">
+            </div>
+            <div class="step-content">
+              <p>Plan Perfectly</p>
+              <small>Coordinate your itinerary, budget, and schedule with your group</small>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-3">
+          <div class="steps-item d-flex justify-content-start align-items-start d-md-block">
+            <div class="step-icon">
+              <img src="<?php echo get_parent_theme_file_uri()?>/assets/images/icons/step-02.svg" alt=""
+                class="img-fluid">
+            </div>
+            <div class="step-content">
+              <p>Share Seamlessly</p>
+              <small>Share your dream trip details with our experts to bring your adventure to life.</small>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-3">
+          <div class="steps-item d-flex justify-content-start align-items-start d-md-block ">
+            <div class="step-icon">
+              <img src="<?php echo get_parent_theme_file_uri()?>/assets/images/icons/step-03.svg" alt=""
+                class="img-fluid">
+            </div>
+            <div class="step-content">
+              <p>Get Guided</p>
+              <small>Receive a custom travel package tailored by our specialists.</small>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-3">
+          <div class="steps-item d-flex justify-content-start align-items-start d-md-block">
+            <div class="step-icon">
+              <img src="<?php echo get_parent_theme_file_uri()?>/assets/images/icons/step-04.svg" alt=""
+                class="img-fluid">
+            </div>
+            <div class="step-content">
+              <p>Travel Together</p>
+              <small>Enjoy a personalized experience with the expertise of local guides.</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
