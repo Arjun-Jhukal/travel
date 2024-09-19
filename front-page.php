33e219c7e8b8ff2aaf948bffee7be5e71d79b86f
jhukal9@gmail.com <jhukal9@gmail.com>
2024-09-19 14:39:22 +0545

    updated the activity section

--- front-page.php
+++ front-page.php
@@ -6,6 +6,8 @@ get_template_part('template-parts/landing-page/steps',null);
 get_template_part('template-parts/landing-page/destinations',null);
 get_template_part('template-parts/landing-page/key-feature',null);
 get_template_part('template-parts/landing-page/popular-package',null);
+get_template_part('template-parts/landing-page/activities',null);
+get_template_part('template-parts/landing-page/testimonial',null);
 ?>
 
 <?php get_footer()?>
---
+++ template-parts/landing-page/activities.php
@@ -0,0 +1,88 @@
+<section class="activities ">
+  <div class="container">
+    <div class="section-title">
+      <h2 class="text-center">Experience Our Diverse
+        <span>Activities and Experiences</span>
+      </h2>
+    </div>
+    <div class="activities-grid">
+      <div class="activity-item featured">
+        <a href="#">
+          <div class="ai-image">
+            <img src="<?php echo get_parent_theme_file_uri()?>/assets/images/placeholder-14.png" alt=""
+              class="img-fluid">
+          </div>
+          <div class="ai-content">
+            <h6>Trekking/Hiking</h6>
+            <p class="sm-text">24 Tours</p>
+          </div>
+        </a>
+      </div>
+      <div class="activity-item featured ">
+        <a href="#">
+          <div class="ai-image">
+            <img src="<?php echo get_parent_theme_file_uri()?>/assets/images/placeholder-15.png" alt=""
+              class="img-fluid">
+          </div>
+          <div class="ai-content">
+            <h6>Wildlife & Nature</h6>
+            <p class="sm-text">24 Tours</p>
+          </div>
+        </a>
+      </div>
+      <div class="activity-item ">
+        <a href="#">
+          <div class="ai-image">
+            <img src="<?php echo get_parent_theme_file_uri()?>/assets/images/placeholder-16.png" alt=""
+              class="img-fluid">
+          </div>
+          <div class="ai-content">
+            <h6>Tours & Sighteen</h6>
+            <p class="sm-text">24 Tours</p>
+          </div>
+        </a>
+      </div>
+      <div class="activity-item ">
+        <a href="#">
+          <div class="ai-image">
+            <img src="<?php echo get_parent_theme_file_uri()?>/assets/images/placeholder-17.png" alt=""
+              class="img-fluid">
+          </div>
+          <div class="ai-content">
+            <h6>Spiritual Religious</h6>
+            <p class="sm-text">24 Tours</p>
+          </div>
+        </a>
+      </div>
+      <div class="activity-item ">
+        <a href="#">
+          <div class="ai-image">
+            <img src="<?php echo get_parent_theme_file_uri()?>/assets/images/placeholder-18.png" alt=""
+              class="img-fluid">
+          </div>
+          <div class="ai-content">
+            <h6>Water Sports</h6>
+            <p class="sm-text">24 Tours</p>
+          </div>
+        </a>
+      </div>
+      <div class="activity-item ">
+        <a href="#">
+          <div class="ai-image">
+            <img src="<?php echo get_parent_theme_file_uri()?>/assets/images/placeholder-19.png" alt=""
+              class="img-fluid">
+          </div>
+          <div class="ai-content">
+            <h6>Hunting</h6>
+            <p class="sm-text">24 Tours</p>
+          </div>
+        </a>
+      </div>
+
+    </div>
+
+    <p class="text-center section-cta">Couldn’t find the Activities you are looking for? <a href="#"
+        class="bh-btn bh-btn-underlined">View all
+        Activities</a></p>
+  </div>
+</section>
