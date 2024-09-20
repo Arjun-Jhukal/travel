6e38f62ef237e2c97fc0ce6771dc0fe9416a7ada
jhukal9@gmail.com <jhukal9@gmail.com>
2024-09-20 13:47:58 +0545

    updated the enqueue for gsap

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -22,6 +22,8 @@ wp_enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dis
 array('jquery'), 5.1, true);
 wp_enqueue_script('flatpickr-script', 'https://cdnjs.cloudflare.com/ajax/libs/flatpickr/4.6.13/flatpickr.min.js',
 array(), '4.6.13', true);
+wp_enqueue_script('gsap', 'https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js', array(), null, true);
+wp_enqueue_script('scrolltrigger', 'https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/ScrollTrigger.min.js', array(), null, true);
 wp_enqueue_script('typed', 'https://cdn.jsdelivr.net/npm/typed.js@2.0.11', array('jquery'), 1.6, true);
 wp_enqueue_script('slick', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js', array('jquery'),
 null, true);
