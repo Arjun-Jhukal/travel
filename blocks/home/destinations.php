3a08a65ee9eea811634dc774e84ea8af09c4804d
rajshah <rajshah.makura@gmail.com>
2024-09-20 15:41:32 +0545

    new blocks

---
+++ blocks/home/destinations.php
@@ -0,0 +1,61 @@
+<section class="destinations section-gap">
+  <div class=" container">
+    <div class="section-title d-flex justify-content-between align-items-center flex-wrap">
+      <h2>Uncover the Hottest <span>Destinations of the Season</span></h2>
+      <a href="#" class="bh-btn bh-btn-filled-dark">View All</a>
+    </div>
+    <div class="destinations-slider-wrapper">
+      <div class="destinations-slider" id="destination-slider">
+        <div class="destination-item">
+          <a href="#">
+            <div class="dsi-image">
+              <img src="<?php echo get_parent_theme_file_uri()?>/assets/images/placeholder-05.png" alt=""
+                class="img-fluid">
+            </div>
+            <div class="dsi-content d-flex justify-content-between align-items-center">
+              <h6>Nepal</h6>
+              <p class="sm-text">24 Tours</p>
+            </div>
+          </a>
+        </div>
+        <div class="destination-item">
+          <a href="#">
+            <div class="dsi-image">
+              <img src="<?php echo get_parent_theme_file_uri()?>/assets/images/placeholder-04.png" alt=""
+                class="img-fluid">
+            </div>
+            <div class="dsi-content d-flex justify-content-between align-items-center">
+              <h6>India</h6>
+              <p class="sm-text">24 Tours</p>
+            </div>
+          </a>
+        </div>
+        <div class="destination-item">
+          <a href="#">
+            <div class="dsi-image">
+              <img src="<?php echo get_parent_theme_file_uri()?>/assets/images/placeholder-03.png" alt=""
+                class="img-fluid">
+            </div>
+            <div class="dsi-content d-flex justify-content-between align-items-center">
+              <h6>Bhutan</h6>
+              <p class="sm-text">24 Tours</p>
+            </div>
+          </a>
+        </div>
+        <div class="destination-item">
+          <a href="#">
+            <div class="dsi-image">
+              <img src="<?php echo get_parent_theme_file_uri()?>/assets/images/placeholder-02.png" alt=""
+                class="img-fluid">
+            </div>
+            <div class="dsi-content d-flex justify-content-between align-items-center">
+              <h6>Indonesia</h6>
+              <p class="sm-text">24 Tours</p>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div class="slider-dots"></div>
+    </div>
+  </div>
+</section>
