11dea1ed93e79aa07e099dc5ef33f5a84d5b89d0
rajshah <rajshah.makura@gmail.com>
2024-09-22 11:31:23 +0545

    reviews header and setup home added

--- blocks/home/reviews.php
+++ blocks/home/reviews.php
@@ -1,5 +1,5 @@
 <!-- Reviews of Bold Himalaya Tours -->
-<!-- <?php
+<?php
 $enable_reviews = get_field('enable_reviews');
 $reviews_title = get_field('reviews_title');
 if ($enable_reviews):
@@ -7,12 +7,14 @@ if ($enable_reviews):
   <section class="testimonials section-gap">
     <div class="container">
       <div class="section-title">
-        <h2 class="text-center"><?php echo $reviews_title;?>
+        <h2 class="text-center">
+          <?php echo $reviews_title; ?>
         </h2>
       </div>
-
       <div class="testimonials-wrapper">
         <h2><span>Testimonials Yet To Come...</span></h2>
       </div>
-    <?php endif; ?> -->
+    </div>
+  </section>
+<?php endif; ?>
 <!-- !Reviews of Bold Himalaya Tours -->
