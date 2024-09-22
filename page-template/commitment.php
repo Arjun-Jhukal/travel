08c9f047330dac296669338f3738d589353a46e5
jhukal9@gmail.com <jhukal9@gmail.com>
2024-09-22 11:33:50 +0545

    updated the commitment page

--- page-template/commitment.php
+++ page-template/commitment.php
@@ -12,8 +12,7 @@
 
 get_template_part("/template-parts/breadcrumb",null);
 get_template_part("/template-parts/general-page-intro",null);
-get_template_part("/template-parts/awards-page/award-listing",null);
-get_template_part("/template-parts/awards-page/certificate-listing",null);
+get_template_part("/template-parts/commitment-page/commitment-message",null);
 get_template_part("/template-parts/global-cta",null);
 
 ?>
