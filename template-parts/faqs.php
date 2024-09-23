f158994fbbdb0918a1b30e7443e7a4152e30da67
jhukal9@gmail.com <jhukal9@gmail.com>
2024-09-23 12:37:30 +0545

    udpated the faq section

---
+++ template-parts/faqs.php
@@ -0,0 +1,108 @@
+<section class="faqs section-padding-y" id="travel-guide">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4">
+        <div class="section-title">
+          <h2>Find Your Answers In
+            <span>Our Nepal FAQs</span>
+          </h2>
+          <p>Find answers to your most pressing questions about traveling in Nepal, including details on itineraries,
+            safety, cultural tips, and more to help you plan a perfect journey.</p>
+        </div>
+        <div class="faq-cta d-none d-lg-block">
+          <p class="sm-text">Still have questions? Feel free to contact us anytime for further assistance.</p>
+          <a href="/contact" class="bh-btn bh-btn-underlined">Contact Us</a>
+        </div>
+      </div>
+      <div class="col-lg-8">
+        <div class="faq-items">
+          <div class="faq-item">
+            <div class="faq-wrapper">
+              <div class="faq-title">
+                <strong>Is it safe to visit Nepal?</strong>
+              </div>
+              <div class="faq-content-wrapper">
+                <div class="faq-content">
+                  <p>Nepal is a really secure place for tourists to explore. The people there think of visitors as
+                    special
+                    guests, so they are kind friendly, and welcoming. There is no unfair treatment, wrongdoing, or
+                    aggression, even among the locals. That's why Nepal is a great choice for a safe travel destination.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="faq-item">
+            <div class="faq-wrapper">
+              <div class="faq-title">
+                <strong>What can you expect the weather to be like in Nepal?</strong>
+              </div>
+              <div class="faq-content-wrapper">
+                <div class="faq-content">
+                  <p>Nepal is a really secure place for tourists to explore. The people there think of visitors as
+                    special
+                    guests, so they are kind friendly, and welcoming. There is no unfair treatment, wrongdoing, or
+                    aggression, even among the locals. That's why Nepal is a great choice for a safe travel destination.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="faq-item">
+            <div class="faq-wrapper">
+              <div class="faq-title">
+                <strong>What is the best season for trekking in Nepal?</strong>
+              </div>
+              <div class="faq-content-wrapper">
+                <div class="faq-content">
+                  <p>Nepal is a really secure place for tourists to explore. The people there think of visitors as
+                    special
+                    guests, so they are kind friendly, and welcoming. There is no unfair treatment, wrongdoing, or
+                    aggression, even among the locals. That's why Nepal is a great choice for a safe travel destination.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="faq-item">
+            <div class="faq-wrapper">
+              <div class="faq-title">
+                <strong>What are the major tourist places in Nepal?</strong>
+              </div>
+              <div class="faq-content-wrapper">
+                <div class="faq-content">
+                  <p>Nepal is a really secure place for tourists to explore. The people there think of visitors as
+                    special
+                    guests, so they are kind friendly, and welcoming. There is no unfair treatment, wrongdoing, or
+                    aggression, even among the locals. That's why Nepal is a great choice for a safe travel destination.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="faq-item">
+            <div class="faq-wrapper">
+              <div class="faq-title">
+                <strong>What are the popular local foods to try in Nepal?</strong>
+              </div>
+              <div class="faq-content-wrapper">
+                <div class="faq-content">
+                  <p>Nepal is a really secure place for tourists to explore. The people there think of visitors as
+                    special
+                    guests, so they are kind friendly, and welcoming. There is no unfair treatment, wrongdoing, or
+                    aggression, even among the locals. That's why Nepal is a great choice for a safe travel destination.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="faq-cta d-lg-none">
+          <p class="sm-text">Still have questions? Feel free to contact us anytime for further assistance.</p>
+          <a href="/contact" class="bh-btn bh-btn-underlined">Contact Us</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
