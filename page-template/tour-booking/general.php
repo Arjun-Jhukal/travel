39b9a34e93aef1d21b55c3a87f5b4471b03269d6
jhukal9@gmail.com <jhukal9@gmail.com>
2024-09-26 17:36:18 +0545

    updated the form pages

---
+++ page-template/tour-booking/general.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Template Name: Book Tour General Template
+ * 
+ */
+
+ get_header();
+?>
+
+<?php
+
+get_template_part("/template-parts/breadcrumb",null);
+
+
+?>
+
+<?php get_footer()?>
