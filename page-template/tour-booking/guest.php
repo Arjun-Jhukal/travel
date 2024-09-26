147be58b59e68a25839492ae3f5de2620ecdbce8
jhukal9@gmail.com <jhukal9@gmail.com>
2024-09-26 17:36:18 +0545

    updated the form pages

---
+++ page-template/tour-booking/guest.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Template Name: Guest Template
+ * 
+ */
+
+ get_header();
+?>
+
+<?php
+
+get_template_part("/template-parts/breadcrumb",null);
+
+
+?>
+
+<?php get_footer()?>
---
+++ page-template/tour-booking/lead-traveller.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Template Name: Lead Traveler Template
+ * 
+ */
+
+ get_header();
+?>
+
+<?php
+
+get_template_part("/template-parts/breadcrumb",null);
+
+
+?>
+
+<?php get_footer()?>
